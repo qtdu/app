716eb8b0d5daafc8ececb8acc737b097926699cb
qtdu <116353460+qtdu@users.noreply.github.com>
2022-10-27 17:44:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -614,7 +614,7 @@ function QT001(){
 		    //Iy = img.offsetTop;
 			//Ix = getOffset(img).left;
 		    	//Iy = getOffset(img).top;
-			Ix = (W - img.offsetWidth)/2;
+			Ix = (W - img.offsetWidth)/2 - Zw/2;
 		    	Iy = EID('header').offsetHeight;
 			  
 			  //alert(Ix + "_" + Iy);
