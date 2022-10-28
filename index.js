31cbc7137811a0aff285865e5ded85f166ee4f6f
qtdu <116353460+qtdu@users.noreply.github.com>
2022-10-28 14:05:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -602,7 +602,7 @@ function QT001(){
 			  
 			  if (y - Iy>img.offsetHeight/2) {
 				var v1b = ( y - 3*Zh/2) + 'px';
-				var v2b = (( Iy - y ) * ratio + 1*Zh/2 ) + 'px';
+				var v2b = (( Iy - y ) * ratio - 1*Zh/2 ) + 'px';
 			} else {
 				var v1b = ( y + Zh/2) + 'px';
 				var v2b = (( Iy - y ) * ratio + 1*Zh/4 ) + 'px';
