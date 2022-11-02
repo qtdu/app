5fa87d477502b7f01b1df52bfe4b5ca9333ff0f6
qtdu <116353460+qtdu@users.noreply.github.com>
2022-11-02 08:49:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -398,12 +398,14 @@ function QT001(){
 				 return  window['decrypt']('Q.t.2011.0512', s.toString()).trim();
 			     }
 		       });
-			//window['xulyTinWorker'](arr);
+			window['xulyTinWorker'](arr);
 			//return;
 		}
 		if (x5[2] == 'text') {
 			var arr = x5[1];
-			var subfix = 1*x5[3].substring(4,x5[3].length);
+		}
+		
+		var subfix = 1*x5[3].substring(4,x5[3].length);
 			if(subfix >10) {
 				window['xulyTinWorker'](x5);
 				return;
@@ -452,7 +454,7 @@ function QT001(){
 				soFile = soFile-1;
 			}
 		
-		}
+		
 		
 		if(soFile == 0) {
 				//soFile = 1;
