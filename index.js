8a5ee254a2938e0053c25c8b51dc3ea6c8b84368
qtdu <116353460+qtdu@users.noreply.github.com>
2022-11-05 16:11:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,9 +33,11 @@ function EID(NameID) {return document.getElementById(NameID);}
     //
     window.addEventListener('message', function(event) {
       //alert("Message received from the parent: " + event.data); // Message received from parent
-	if (typeof window['xulyTinIframe'] != 'undefined' && window['xulyTinIframe'] != null) {
-    		window['xulyTinIframe'](event.data);
-	}
+	//if (typeof window['xulyTinIframe'] != 'undefined' && window['xulyTinIframe'] != null) {
+    	//	window['xulyTinIframe'](event.data);
+	//}
+	    try{ window['xulyTinIframe'](event.data); }
+	    catch(err){}
 
     });
 
