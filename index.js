1184d9ff874ddd0c6cadbb9b0bb8496e7bf55fd9
qtdu <116353460+qtdu@users.noreply.github.com>
2022-11-08 22:20:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -761,9 +761,9 @@ function EID(NameID) {return document.getElementById(NameID);}
         Zh = zoom.offsetHeight;
 
         var v1a = ( x ) + 'px';
-        var v1b = ( y - Iy) + 'px';
-        var v2a = (( Ix - x ) * ratio + Zw/2) + 'px ';
-        var v2b = (( Iy - y ) * ratio + Zh/2) + 'px';
+        var v1b = ( y ) + 'px'; //( y - Iy) + 'px';
+        var v2a = (( Ix - x ) * ratio + Zw/4) + 'px '; //(( Ix - x ) * ratio + Zw/2) + 'px ';
+        var v2b = (( Iy - y ) * ratio + Zh/4) + 'px'; //(( Iy - y ) * ratio + Zh/2) + 'px';
 
         zoom.style.left = v1a;
         zoom.style.top = v1b;  
@@ -779,7 +779,7 @@ function EID(NameID) {return document.getElementById(NameID);}
       }  
 
       window.window['onLoadImage'] = function() {
-        var hImage = ECN('content','image2zoom_container')[0].offsetHeight;
+        var hImage = EID('image2zoom').offsetHeight;
       root.style.setProperty('--w-zoom_image', hImage/4 + 'px');
 
         zoom.style.backgroundImage = 'url(' + img.src + ')';
