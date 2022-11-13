64156f450b9b938098b7dfda5d3163a053bc8127
qtdu <116353460+qtdu@users.noreply.github.com>
2022-11-13 07:27:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1454,7 +1454,189 @@ function EID(NameID) {return document.getElementById(NameID);}
 		  }
 
 
-		}
+		} //ket thuc func_ScanBarcode
+	  
+	  	
+	  	window.window['func_dragElement'] = function(elmnt) {
+
+		     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+		     if (EID(elmnt.id + "header")) {
+
+		     EID(elmnt.id + "header").onmousedown = dragMouseDown;
+		     } else {
+
+		     elmnt.onmousedown = dragMouseDown;
+		     }
+
+		     function dragMouseDown(e) {
+		     e = e || window.event;
+		     e.preventDefault();
+
+		     pos3 = e.clientX;
+		     pos4 = e.clientY;
+		     document.onmouseup = closeDragElement;
+
+		     document.onmousemove = elementDrag;
+		     }
+
+		     function elementDrag(e) {
+		     e = e || window.event;
+		     e.preventDefault();
+
+		     pos1 = pos3 - e.clientX;
+		     pos2 = pos4 - e.clientY;
+		     pos3 = e.clientX;
+		     pos4 = e.clientY;
+
+		     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+		     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+		     }
+
+		     function closeDragElement() {
+
+		     document.onmouseup = null;
+		     document.onmousemove = null;
+		     }
+
+
+
+		} //ket thuc hàm func_dragElement
+	  
+	  
+	  	window.window['func_swiped_events'] = function() {
+  
+			(function (window, document) {
+
+			    'use strict';
+
+			    // patch CustomEvent to allow constructor creation (IE/Chrome)
+			    if (typeof window.CustomEvent !== 'function') {
+
+				window.CustomEvent = function (event, params) {
+
+				    params = params || { bubbles: false, cancelable: false, detail: undefined };
+
+				    var evt = document.createEvent('CustomEvent');
+				    evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+				    return evt;
+				};
+
+				window.CustomEvent.prototype = window.Event.prototype;
+			    }
+
+			    document.addEventListener('touchstart', handleTouchStart, false);
+			    document.addEventListener('touchmove', handleTouchMove, false);
+			    document.addEventListener('touchend', handleTouchEnd, false);
+
+			    var xDown = null;
+			    var yDown = null;
+			    var xDiff = null;
+			    var yDiff = null;
+			    var timeDown = null;
+			    var startEl = null;
+
+
+			    function handleTouchEnd(e) {
+
+				// if the user released on a different target, cancel!
+				if (startEl !== e.target) return;
+
+				var swipeThreshold = parseInt(getNearestAttribute(startEl, 'data-swipe-threshold', '20'), 10); // default 20px
+				var swipeTimeout = parseInt(getNearestAttribute(startEl, 'data-swipe-timeout', '500'), 10);    // default 500ms
+				var timeDiff = Date.now() - timeDown;
+				var eventType = '';
+				var changedTouches = e.changedTouches || e.touches || [];
+
+				if (Math.abs(xDiff) > Math.abs(yDiff)) { // most significant
+				    if (Math.abs(xDiff) > swipeThreshold && timeDiff < swipeTimeout) {
+					if (xDiff > 0) {
+					    eventType = 'swiped-left';
+					}
+					else {
+					    eventType = 'swiped-right';
+					}
+				    }
+				}
+				else if (Math.abs(yDiff) > swipeThreshold && timeDiff < swipeTimeout) {
+				    if (yDiff > 0) {
+					eventType = 'swiped-up';
+				    }
+				    else {
+					eventType = 'swiped-down';
+				    }
+				}
+
+				if (eventType !== '') {
+
+				    var eventData = {
+					dir: eventType.replace(/swiped-/, ''),
+					touchType: (changedTouches[0] || {}).touchType || 'direct',
+					xStart: parseInt(xDown, 10),
+					xEnd: parseInt((changedTouches[0] || {}).clientX || -1, 10),
+					yStart: parseInt(yDown, 10),
+					yEnd: parseInt((changedTouches[0] || {}).clientY || -1, 10)
+				    };
+
+				    // fire `swiped` event event on the element that started the swipe
+				    startEl.dispatchEvent(new CustomEvent('swiped', { bubbles: true, cancelable: true, detail: eventData }));
+
+				    // fire `swiped-dir` event on the element that started the swipe
+				    startEl.dispatchEvent(new CustomEvent(eventType, { bubbles: true, cancelable: true, detail: eventData }));
+				}
+
+				// reset values
+				xDown = null;
+				yDown = null;
+				timeDown = null;
+			    }
+
+			    function handleTouchStart(e) {
+
+				// if the element has data-swipe-ignore="true" we stop listening for swipe events
+				if (e.target.getAttribute('data-swipe-ignore') === 'true') return;
+
+				startEl = e.target;
+
+				timeDown = Date.now();
+				xDown = e.touches[0].clientX;
+				yDown = e.touches[0].clientY;
+				xDiff = 0;
+				yDiff = 0;
+			    }
+
+
+			    function handleTouchMove(e) {
+
+				if (!xDown || !yDown) return;
+
+				var xUp = e.touches[0].clientX;
+				var yUp = e.touches[0].clientY;
+
+				xDiff = xDown - xUp;
+				yDiff = yDown - yUp;
+			    }
+
+
+			    function getNearestAttribute(el, attributeName, defaultValue) {
+
+				// walk up the dom tree looking for attributeName
+				while (el && el !== document.documentElement) {
+
+				    var attributeValue = el.getAttribute(attributeName);
+
+				    if (attributeValue) {
+					return attributeValue;
+				    }
+
+				    el = el.parentNode;
+				}
+
+				return defaultValue;
+			    }
+
+			}(window, document));
+
+			} //ket thuc hàm func_swiped_events
 	  
 	  
 	}
