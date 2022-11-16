a39fc8f2557406fe4015e20f187e6d3fc7974825
qtdu <116353460+qtdu@users.noreply.github.com>
2022-11-16 10:40:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
+function lzw_encode(s) {
+    var dict = {};
+    var data = (s + "").split("");
+    var out = [];
+    var currChar;
+    var phrase = data[0];
+    var code = 256;
+    for (var i=1; i<data.length; i++) {
+        currChar=data[i];
+        if (dict[phrase + currChar] != null) {
+            phrase += currChar;
+        }
+        else {
+            out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+            dict[phrase + currChar] = code;
+            code++;
+            phrase=currChar;
+        }
+    }
+    out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+    for (var i=0; i<out.length; i++) {
+        out[i] = String.fromCharCode(out[i]);
+    }
+    return out.join("");
+}
+
+// Decompress an LZW-encoded string
+function lzw_decode(s) {
+    var dict = {};
+    var data = (s + "").split("");
+    var currChar = data[0];
+    var oldPhrase = currChar;
+    var out = [currChar];
+    var code = 256;
+    var phrase;
+    for (var i=1; i<data.length; i++) {
+        var currCode = data[i].charCodeAt(0);
+        if (currCode < 256) {
+            phrase = data[i];
+        }
+        else {
+           phrase = dict[currCode] ? dict[currCode] : (oldPhrase + currChar);
+        }
+        out.push(phrase);
+        currChar = phrase.charAt(0);
+        dict[code] = oldPhrase + currChar;
+        code++;
+        oldPhrase = phrase;
+    }
+    return out.join("");
+}
 
 function EID(NameID) {return document.getElementById(NameID);}
   function ECN(target,NameClass) {
@@ -666,7 +717,7 @@ function EID(NameID) {return document.getElementById(NameID);}
 	    
 	if(arr.indexOf('content2b') < 0) {
 		alert(arr);
-		arr = LZUTF8.decompress(arr);
+		arr = lzw_decode(arr)
 		alert(arr);
 	}
 	    
