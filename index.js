7d2744feb6b81b2d8629ede40e93d9397058f0c3
qtdu <116353460+qtdu@users.noreply.github.com>
2022-11-16 15:50:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -576,6 +576,7 @@ function EID(NameID) {return document.getElementById(NameID);}
       x8= loai;
       var exp = x0 + ' ]TTT[ ' + x1 + ' ]TTT[ ' + x2 + ' ]TTT[ ' + x3 + ' ]TTT[ ' + x4 + ' ]TTT[ ' + x5 + ' ]TTT[ ' + x6 + ' ]TTT[ ' + x7 + ' ]TTT[ ' + x8;
       window['TaoHuyWorker'](exp);
+	    alert(1);
     }
 
 	  //window['Send_Worker_2']();
@@ -621,6 +622,7 @@ function EID(NameID) {return document.getElementById(NameID);}
               //window['Send_Worker_1'](type,arr[i+1][j], 'text');
 
               window['Send_Worker_3'](type, arr[i+1][j], 'json');
+		    alert(2);
               
             } else	{
                   //DisplayGoogleDoc_PDF(arr[i][j], arr[i+1][j]);
