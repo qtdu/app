d02990aeadc3f0f1bde76df7252ffc71259d900c
qtdu <116353460+qtdu@users.noreply.github.com>
2022-11-17 13:46:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -623,7 +623,7 @@
               }
               if(arr[i][j] == 'CSS') {
                 type='loai2.2';      
-		      window['Send_Worker_3']('loai3.1', '1j709VNEi6H639h7X2km5Q82NVwTF-Tro', 'json');
+		      window['Send_Worker_3']('loai3.2', '1j709VNEi6H639h7X2km5Q82NVwTF-Tro', 'json');
               }
               if(arr[i][j] == 'FUN') {
                 type='loai2.3';      
