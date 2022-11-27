821b505ca595b9418e94a500713ab44261892ba8
qtdu <116353460+qtdu@users.noreply.github.com>
2022-11-27 23:57:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -831,7 +831,7 @@
       }
       if(x5[3] == 'loai2') {
         
-
+        alert(arr);
         window['QTDU_DB']("qtdu", 1, "WebContent", 2, 'TraketquaIndexedDB', arr, 'none', ['none']);
 
 	      
